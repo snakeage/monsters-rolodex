525515b7dcc8e701eaff3f250b52b243004c3878
Alexandr <snakeage@gmail.com>
2019-12-09 12:26:49 +0300

    Lesson5

---
+++ Lesson5/Для ДЗ №5/js/script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+let menu = document.getElementsByClassName("menu")[0],
+    menuItem = document.getElementsByClassName("menu-item"),
+    title = document.getElementById("title"),
+    adv = document.getElementsByClassName("adv")[0],
+    promptForApple = document.querySelector("#prompt"),
+    menuItemLi = document.createElement("li");
+
+menu.insertBefore(menuItem[2], menuItem[1]);
+
+menuItemLi.classList.add("menu-item");
+menuItemLi.textContent = "Пятый элемент";
+menu.appendChild(menuItemLi);
+
+
+document.body.style.backgroundImage = "url('img/apple_true.jpg')";
+
+title.textContent = "Мы продаем только подлинную технику Apple";
+
+adv.remove();
+
+let yourOpinion = prompt("Ваше отнршение к технике Apple?");
+promptForApple.textContent = yourOpinion;
