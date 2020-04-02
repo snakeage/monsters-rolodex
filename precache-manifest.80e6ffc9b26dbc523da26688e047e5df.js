6db348453e9c6a85be79b1f2f606818c49f02307
Alexander <Snakeage@gmail.com>
2020-04-02 18:38:58 +0300

    Updates

---
+++ precache-manifest.80e6ffc9b26dbc523da26688e047e5df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51b4cebe8cae3b15c5a257ecd64f1137",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "811bbdad8274d498bc27",
+    "url": "/monsters-rolodex/static/css/main.60e508b9.chunk.css"
+  },
+  {
+    "revision": "147f1327e7798733efc4",
+    "url": "/monsters-rolodex/static/js/2.d562cfe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.d562cfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "811bbdad8274d498bc27",
+    "url": "/monsters-rolodex/static/js/main.e894d1f0.chunk.js"
+  },
+  {
+    "revision": "83fef09b6b4b91e8e383",
+    "url": "/monsters-rolodex/static/js/runtime-main.6036d2a0.js"
+  }
+]);
